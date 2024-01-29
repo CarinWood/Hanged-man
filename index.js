bdd437dc08cf3106911de17560b8ec71f68be4ac
CarinWood <carin.wood.85@gmail.com>
2024-01-29 12:41:23 +0100

    created a word picker when clicking start button

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
   //  scaffold.style.opacity = '0'
 });
 
-
 const wordArray = ['bananpaj', 'lingonsylt', 'glasögon', 'blåbär', 'vitmögelost', 'ansiktslyft', 'trädgårdsredskap']
+let correctWord = ''
+
+let startBtn = document.querySelector('.start-btn')
+startBtn.addEventListener('click', function() {
+    let num = Math.floor((Math.random() * 7))
+    correctWord = wordArray[num]
+    setPlayboard()
+})
+
+
+function setPlayboard() {
+    
+}
+
