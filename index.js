d5559ad9f116151b54248558e1a2eae6feccba14
CarinWood <carin.wood.85@gmail.com>
2024-01-29 13:28:37 +0100

    game function

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const wordArray = ['bananpaj', 'lingonsylt', 'glasögon', 'blåbär', 'vitmögelost', 'ansiktslyft', 'trädgårdsredskap']
 let correctWord = ''
+let tiles = 0
+let playBoard = document.querySelector('.play-board')
+let instruction = document.querySelector('.instruction')
+let letterBox = document.querySelector('.used-letters')
 
 let startBtn = document.querySelector('.start-btn')
 startBtn.addEventListener('click', function() {
@@ -15,8 +19,31 @@ startBtn.addEventListener('click', function() {
     setPlayboard()
 })
 
+function addTiles() {
+    for (let i = 0; i < correctWord.length; i++) {
+        let tile = document.createElement('p')
+        tile.classList.add('tile')
+        playBoard.appendChild(tile)
+    }
+}
+
+function startGame() {
+    document.addEventListener('keydown', function(event) {
+        // Hämta tangenten som trycktes ner
+        var keyPressed = event.key;
+        if(correctWord.includes(keyPressed)) {
+            console.log('bokstaven finns i ordet!')
+        } else {
+            letterBox.innerText += keyPressed
+        }
+    });
+}
 
 function setPlayboard() {
-    
+    startBtn.classList.add('hide')
+    instruction.classList.add('show')
+    console.log(correctWord)
+    addTiles()
+    startGame()
 }
 
