8ae53824126613fa47780be33cebbc3db460c58e
CarinWood <carin.wood.85@gmail.com>
2024-01-29 10:32:22 +0100

    wordArray added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     var svgObjekt = document.getElementById("svgfile");
     var svgInnehall = svgObjekt.contentDocument;
-
-    // Exempel: Ändra färgen på "head" till röd
     var scaffold = svgInnehall.getElementById("scaffold");
-    scaffold.setAttribute("fill", "red");
+  //  scaffold.style.opacity = '0'
 });
+
+
+const wordArray = ['bananpaj', 'lingonsylt', 'glasögon', 'blåbär', 'vitmögelost', 'ansiktslyft', 'trädgårdsredskap']
