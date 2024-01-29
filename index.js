113c1b57bff7bd9a089d5c4a224fb7ba45eb64f8
CarinWood <carin.wood.85@gmail.com>
2024-01-29 13:52:23 +0100

    made a function with switch for each part of the hanged man

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
     var svgObjekt = document.getElementById("svgfile");
     var svgInnehall = svgObjekt.contentDocument;
-    var scaffold = svgInnehall.getElementById("scaffold");
-  //  scaffold.style.opacity = '0'
+    ground = svgInnehall.getElementById("ground")
+    ground.style.opacity = '0'
+    scaffold = svgInnehall.getElementById("scaffold");
+    scaffold.style.opacity = '0'
+    head = svgInnehall.getElementById("head")
+    head.style.opacity = '0'
+    body = svgInnehall.getElementById("body")
+    body.style.opacity = '0'
+    arms = svgInnehall.getElementById("arms")
+    arms.style.opacity = '0'
+    legs = svgInnehall.getElementById("legs")
+    legs.style.opacity = '0'
 });
 
+
 const wordArray = ['bananpaj', 'lingonsylt', 'glasögon', 'blåbär', 'vitmögelost', 'ansiktslyft', 'trädgårdsredskap']
+let ground
+let scaffold
+let head
+let body
+let arms
+let legs
+let strike = 0
 let correctWord = ''
 let tiles = 0
 let playBoard = document.querySelector('.play-board')
 let instruction = document.querySelector('.instruction')
 let letterBox = document.querySelector('.used-letters')
+let allowedLetters = 'abcdefghijklmnopqrstuvwxyzåäö'
 
 let startBtn = document.querySelector('.start-btn')
 startBtn.addEventListener('click', function() {
@@ -27,6 +46,24 @@ function addTiles() {
     }
 }
 
+function hang(strike) {
+    switch(strike) {
+        case 1: ground.style.opacity = '1'
+        break
+        case 2: scaffold.style.opacity = '1'
+        break
+        case 3: head.style.opacity = '1'
+        break
+        case 4: body.style.opacity = '1'
+        break
+        case 5: arms.style.opacity = '1'
+        break
+        case 6: legs.style.opacity = '1'
+        break
+        default: 'Whatever'
+    }
+}
+
 function startGame() {
     document.addEventListener('keydown', function(event) {
         // Hämta tangenten som trycktes ner
@@ -34,7 +71,15 @@ function startGame() {
         if(correctWord.includes(keyPressed)) {
             console.log('bokstaven finns i ordet!')
         } else {
-            letterBox.innerText += keyPressed
+            if(allowedLetters.includes(keyPressed)) {
+                letterBox.innerText += keyPressed
+                strike++
+                console.log(strike)
+                hang(strike)
+            } else {
+                console.log('Tangenten innehåller inte en tillåten bokstav: ' + keyPressed)
+            }
+            
         }
     });
 }
